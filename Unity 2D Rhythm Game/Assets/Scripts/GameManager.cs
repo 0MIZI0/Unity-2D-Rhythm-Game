697d7fec51393ab31ccc1569461472381f003b2c
0MIZI0 <jimin0469@gmail.com>
2021-05-19 20:57:00 +0900

    1.02
    판정선 제작 및 노트 판정 구현

---
+++ Unity 2D Rhythm Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //single tone 
+	public static GameManager instance { get; set; }
+	private void Awake()
+	{
+		if(instance == null) instance = this;
+		else if (instance != this) Destroy(gameObject);
+		
+	}
+	
+	public float noteSpeed;
+	
+	public enum judges { NONE = 0, BAD, GREAT, PERFECT, MISS };
+	/*
+	MISS : 4
+	BAD : 1
+	GREAT : 2
+	PERFECT : 3
+	*/
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
