6b6e76c9b1353d2c983501358e6d1546162103e1
0MIZI0 <jimin0469@gmail.com>
2021-11-03 15:20:56 +0900

    1.09
    Before importing WebView

---
+++ Unity 2D Rhythm Game/Assets/Scripts/Moveto_game_main.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Moveto_game_main : MonoBehaviour
+{
+    public void SceneChange(){
+        SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ Unity 2D Rhythm Game/Assets/Scripts/Moveto_web_main.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Moveto_web_main : MonoBehaviour
+{
+    public void SCeneChange(){
+        SceneManager.LoadScene("WebScene");
+    }
+}
